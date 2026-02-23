9140398addd08c1ebdbfbe47abbec6ff36a2e0a9
alopgau <alopezg@iesrafaelalberti.es>
2026-02-23 13:54:21 +0100

    prueba links

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,17 @@
-
-console.log(document);
-console.log(document.querySelector(".cabecera"));
-console.log(document.querySelectorAll(".cabecera__menu__lista--elemento"));
-const div = document.createElement("div")
-const footer = document.querySelector("footer")
-footer.prepend(div)
+const juegos = document.querySelectorAll(".juego")
+for (let index = 0; index < juegos.length; index++) {
+     juegos[index].setAttribute("id",index);
+}
+for (const juego of juegos) {
+    switch (juego.getAttribute("id")) {
+        case "0":
+            juego.addEventListener("click", () => {
+                window.location.href = "https://www.google.com"
+            })
+            break;
+    
+        default:
+            break;
+    }
+    
+}
